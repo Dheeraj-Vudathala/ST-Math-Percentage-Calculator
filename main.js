1a506ebc8ec85d93d0017bbb4fc902bf8d465d72
Dheeraj-Vudathala <74560258+Dheeraj-Vudathala@users.noreply.github.com>
2021-01-06 18:22:33 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function Submit() {
+    var puzzles = document.getElementById("puzzles").value;
+        if (puzzles > 3268 || puzzles < 0) {
+            document.getElementById("answer").innerHTML = "Please enter a valid number."
+        }
+    else {
+        document.getElementById("answer").innerHTML = ("You have completed " + (puzzles / 32.68).toFixed(0) + "% of ST Math.");
+    }
+}
