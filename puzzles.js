cf47549cf735e2a07e41df66c031e29a614e44d3
Dheeraj-Vudathala <74560258+Dheeraj-Vudathala@users.noreply.github.com>
2021-01-11 09:50:24 -0800

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,9 +1,9 @@
 function Submit() {
     var puzzles = document.getElementById("puzzles").value;
-    if (puzzles > 2986 || puzzles < 0) {
+    if (puzzles > 3268 || puzzles < 0) {
         document.getElementById("answer").innerHTML = "Please enter a valid number."
     }
     else {
-        document.getElementById("answer").innerHTML = "You have completed " + (puzzles / 29.8660714).toFixed(0) + "% of ST Math.";
+        document.getElementById("answer").innerHTML = "You have completed " + (puzzles / 32.68).toFixed(0) + "% of ST Math.";
     }
 }
