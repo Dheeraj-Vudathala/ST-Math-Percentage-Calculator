e30b9f480f31b9846d816b01dc588a74824b37c1
Dheeraj-Vudathala <74560258+Dheeraj-Vudathala@users.noreply.github.com>
2021-01-15 14:12:32 -0800

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,6 +1,4 @@
 function Submit() {
     var puzzles = document.getElementById("puzzles").value;
-    else {
-        document.getElementById("answer").innerHTML = "You have completed " + (puzzles / 32.25).toFixed(0) + "% of ST Math.";
-    }
+    document.getElementById("answer").innerHTML = "You have completed " + (puzzles / 32.25).toFixed(0) + "% of ST Math.";
 }
