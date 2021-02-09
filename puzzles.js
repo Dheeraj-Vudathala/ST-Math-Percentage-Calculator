6926233e6f64701a53d6fcd93144c4fd12556735
Dheeraj-Vudathala <74560258+Dheeraj-Vudathala@users.noreply.github.com>
2021-02-09 09:45:49 -0800

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,9 +1,13 @@
 function Submit() {
     var puzzles = document.getElementById("puzzles").value;
+    alert("puzzles");
     if puzzles > 3546 {
         document.getElementById("answer").innerHTML = "Please enter a valid number.";
+        alert("if puzzles");
     }
+    
     else {
         document.getElementById("answer").innerHTML = "You have completed " + (puzzles / 32.25).toFixed(0) + "% of ST Math.";
+        alert("else puzzles");
     }
 }
